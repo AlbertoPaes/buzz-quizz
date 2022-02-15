64e3e6a968db7485a3526178c8dcb09ae3c6b00c
Alberto <tejotaesi@gmail.com>
2022-02-15 13:56:01 -0300

    Esse commit exibe as perguntas de cada quizz individual

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,6 @@
 const URL_BASE = "https://mock-api.driven.com.br/api/v4/buzzquizz";
 let porcentagemDeAcertos = 0;
 
-
 function disporQuizes(){
   const promessa = axios.get(`${URL_BASE}/quizzes`);
   promessa.then( (resposta) => {
@@ -25,50 +24,75 @@ function irPraTelaQuiz(id){
   const quizz = axios.get(`${URL_BASE}/quizzes/${id}`)
 
   telaInicial.classList.add("hidden");
-  quizz.then( (resposta) => {
-    const quizAtual = resposta.data;
-    const corpo = document.querySelector("body");
-    let caixaDeRespostas = "";
+  quizz.then(telaDeQuizz);
+  quizz.catch(erroAxios);
+}
 
-    quizAtual.questions.map( (item) =>
-    caixaDeRespostas +=
-    `<div class="container-pergunta-individual">
-        <div class="container-titulo-pergunta-individual" style="background-color: ${item.color};">
-            <h2 style=" color: ${item.color >= '#7FFFFF' ? 'black': 'white'}">${item.title}</h2>
+function telaDeQuizz(resposta){
+  const quizzAtual = resposta.data;
+  const corpo = document.querySelector("body");
+  let caixaDeRespostas = "";
+
+  quizzAtual.questions.map( (item) =>
+  caixaDeRespostas +=
+  `<div class="container-pergunta-individual">
+      <div class="container-titulo-pergunta-individual" style="background-color: ${item.color};">
+        <h2 style=" color: ${item.color >= '#7FFFFF' ? 'black': 'white'}">${item.title}</h2>
+      </div>
+      <div class="container-respostas-pergunta-individual">
+        ${exibirRespostasQuizz(item.answers,item.length)}
+      </div>        
+    </div>`)
+      
+  corpo.innerHTML += `
+  <div class="pagina-de-um-quizz">
+    <div class="container-foto-de-capa-quizz">
+        <img src="${quizzAtual.image}"/>
+        <span>${quizzAtual.title}</span>
+    </div>
+    ${caixaDeRespostas}
+
+    <div class="container-fim-quizz hidden">
+        <div class="container-resultado" data-identifier="quizz-result">
+            <h1 class="tituloFimQuizz" >${porcentagemDeAcertos}% de acerto: ${quizzAtual.levels[0].title}</h1>
         </div>
-        <div class="container-respostas-pergunta-individual">
-        </div>        
+        <div class="imagem-e-descricao">
+            <img src=${quizzAtual.levels[0].image}"/>
+            <p class= "textoQuizz">${quizzAtual.levels[0].text}
+            </p>
+        </div>
+        <button class="reiniciar-quizz" onclick="reiniciarQuizz(${quizzAtual.id})">
+            Reiniciar Quizz
+        </button>
+        <button  id='fullReset' class="voltar-home" onclick="voltarTela('.corpo-pagina-inicial', '.pagina-de-um-quizz')">
+            Voltar para home
+        </button>
+    </div>
+  </div> 
+  `
+}
+
+function exibirRespostasQuizz(respostas,qtdDeCadaResposta){
+  const ARRAY_DE_RESPOSTAS = Array(respostas.length).fill("");
+  console.log(ARRAY_DE_RESPOSTAS);
+
+  const NOVO_ARRAY_RESPOSTAS = ARRAY_DE_RESPOSTAS.map( (item) => {
+    `<div class="container-resposta-indivual ${(item.isCorrectAnswer === true) ? "resposta-correta" : "resposta-errada"}" onclick="selecionarResposta(this, ${qtdDeCadaResposta})" data-identifier="answer">
+            <img src="${item.image}"/>
+            <span>${item.text}</span>
     </div>`
-    )
+  });
+  console.log(NOVO_ARRAY_RESPOSTAS);
 
-    corpo.innerHTML += `
-    <div class="pagina-de-um-quizz">
-      <div class="container-foto-de-capa-quizz">
-          <img src="${quizAtual.image}"/>
-          <span>${quizAtual.title}</span>
-      </div>
-      ${caixaDeRespostas}
+  NOVO_ARRAY_RESPOSTAS.sort(randomizador);
+  for (let j = 0; j < respostas.length; j++) {
+    retornoString += respostas[j];
+  }
 
-      <div class="container-fim-quizz hidden">
-          <div class="container-resultado" data-identifier="quizz-result">
-              <h1 class="tituloFimQuizz" >${porcentagemDeAcertos}% de acerto: ${quizAtual.levels[0].title}</h1>
-          </div>
-          <div class="imagem-e-descricao">
-              <img src=${quizAtual.levels[0].image}"/>
-              <p class= "textoQuizz">${quizAtual.levels[0].text}
-              </p>
-          </div>
-          <button class="reiniciar-quizz" onclick="reiniciarQuizz(${quizAtual.id})">
-              Reiniciar Quizz
-          </button>
-          <button  id='fullReset' class="voltar-home" onclick="voltarTela('.corpo-pagina-inicial', '.pagina-de-um-quizz')">
-              Voltar para home
-          </button>
-      </div>
-    </div> 
-    `
-  })
+}
 
+function randomizador() {
+  return Math.random() - 0.5;
 }
 
 function erroAxios(){
