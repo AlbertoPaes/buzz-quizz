1872ec7b9ee924cf008e7286ec6bcd1c409fc21e
Felipe <felipecosta1961@gmail.com>
2022-02-16 00:22:35 -0300

    Este commit é um fix do script.js e implementação da edição de perguntas

--- assets/scripts/criacaoQuizz.js
+++ assets/scripts/criacaoQuizz.js
@@ -23,8 +23,120 @@ function mostrarTelaDePerguntas(perguntas) {
 	for (let i = 0; i < parseInt(perguntas); i++) {
 		containerPerguntas += `
 		<div class="caixa-adicao" onclick="editarPergunta(this)">
-			<h3 class="add-title">Pergunta ${i + 1}</h3>
+			<h3>Pergunta ${i + 1}</h3>
 			<ion-icon class = "pencil" name="create-outline"></ion-icon>
+			<form
+				class = "hidden" 
+				onsubmit="verificarPerguntas(); return false"
+				accept-charset="utf-8"
+				name="info-perguntas" >
+				<div class = "pergunta caixa-input-pergunta">
+					<input 
+						placeholder = "Texto da pergunta"
+						id = "input-box"
+						class="texto-pergunta caixa-input-pergunta"
+						name = "texto_pergunta"
+						type = "text"
+						minlenght = "20"
+						required
+					/>
+					<input 
+						placeholder = "Qual a cor de fundo da pergunta"
+						id = "input-box"
+						class="cor-de-fundo-pergunta caixa-input-pergunta"
+						name = "cor_de_fundo_pergunta"
+						type = "text"
+						required
+					/>
+				</div>
+				<div class = "resposta-correta caixa-input-pergunta">
+					<h3>Resposta correta</h3>
+					<input
+						placeholder = "Resposta Correta"
+						id = "input-box"
+						class="texto-resposta-correta " 
+						name = "texto_resposta_correta"
+						type = "text"
+						minlenght = "1"
+						required
+					/>
+					<input
+						placeholder = "URL da imagem"
+						id = "input-box"
+						class="url-img-resposta-correta"
+						name = "url_img_resposta_correta"
+						type = "url"
+						required
+					/>
+				</div>
+
+				<div class = "respostas-incorretas">
+					<h3>Respostas incorretas</h3>
+					<div class ="resposta-incorreta caixa-input-pergunta">
+						<input
+							placeholder = "Resposta incorreta 1"
+							id = "input-box"
+							class="texto-resposta-incorreta1"
+							name = "texto_resposta_incorreta1"
+							type = "text"
+							minlenght = "1"
+							required
+						/>
+						<input
+							placeholder = "URL da imagem"
+							id = "input-box"
+							class="url-img-resposta-incorreta1"
+							name = "url_img_resposta_incorreta1"
+							type = "url"
+							required
+						/>
+					</div>
+
+					<div class ="resposta-incorreta caixa-input-pergunta">
+						<input
+							placeholder = "Resposta Correta"
+							id = "input-box"
+							class="texto-resposta-correta"
+							name = "texto_resposta_correta"
+							type = "text"
+							minlenght = "1"
+							required
+						/>
+						<input
+							placeholder = "URL da imagem"
+							id = "input-box"
+							class="url-img-resposta-correta"
+							name = "url_img_resposta_correta"
+							type = "url"
+							required
+						/>
+					</div>
+
+					<div class ="resposta-incorreta caixa-input-pergunta">
+						<input
+							placeholder = "Resposta Correta"
+							id = "input-box"
+							class="texto-resposta-correta"
+							name = "texto_resposta_correta"
+							type = "text"
+							minlenght = "1"
+							required
+						/>
+						<input
+							placeholder = "URL da imagem"
+							id = "input-box"
+							class="url-img-resposta-correta"
+							name = "url_img_resposta_correta"
+							type = "url"
+							required
+						/>
+					</div>
+				</div>
+			
+			
+	
+			</form>
+	
 		</div>
 
 		
@@ -32,8 +144,8 @@ function mostrarTelaDePerguntas(perguntas) {
 	}
 
 	telaPerguntas.innerHTML += `
+	<p class = "instrucao">Crie suas perguntas</p>
 		<div class = "conteudo-criacao">
-			<p class = "instrucao">Crie suas perguntas</p>
 			${containerPerguntas}
 			<button type="submit" class="botao-prosseguir">
 				<p class="texto-botao-prosseguir">
@@ -45,4 +157,18 @@ function mostrarTelaDePerguntas(perguntas) {
 
 
 	`;
+}
+
+function editarPergunta(divPergunta) {
+	const ativo = document.querySelector(".ativo");
+	if (ativo !== null) {
+		ativo.classList.remove("ativo");
+		console.log(ativo.children[1]);
+		ativo.children[1].classList.remove("hidden");
+		ativo.children[2].classList.add("hidden");
+	}
+
+	divPergunta.children[1].classList.add("hidden");
+	divPergunta.children[2].classList.remove("hidden");
+	divPergunta.classList.add("ativo");
 }
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,55 +23,56 @@ function disporQuizes() {
 function irPraTelaQuiz(id) {
 	const telaInicial = document.querySelector(".corpo-pagina-inicial");
 	const quizz = axios.get(`${URL_BASE}/quizzes/${id}`);
-
 	telaInicial.classList.add("hidden");
-	quizz.then((resposta) => {
-		const quizAtual = resposta.data;
-		const corpo = document.querySelector("body");
-		let caixaDeRespostas = "";
+	quizz.then(telaDeQuizz);
+	quizz.catch(erroAxios);
+}
+function telaDeQuizz(resposta) {
+	const quizzAtual = resposta.data;
+	const corpo = document.querySelector("body");
+	let caixaDeRespostas = "";
+	quizzAtual.questions.map(
+		(item) =>
+			(caixaDeRespostas += `<div class="container-pergunta-individual">
+		<div class="container-titulo-pergunta-individual" style="background-color: ${
+			item.color
+		};">
+		  <h2 style=" color: ${item.color >= "#7FFFFF" ? "black" : "white"}">${
+				item.title
+			}</h2>
+		</div>
+		<div class="container-respostas-pergunta-individual">
+		  ${exibirRespostasQuizz(item.answers, item.length)}
+		</div>        
+	  </div>`)
+	);
 
-		quizAtual.questions.map(
-			(item) =>
-				(caixaDeRespostas += `<div class="container-pergunta-individual">
-        <div class="container-titulo-pergunta-individual" style="background-color: ${
-					item.color
-				};">
-            <h2 style=" color: ${
-							item.color >= "#7FFFFF" ? "black" : "white"
-						}">${item.title}</h2>
-        </div>
-        <div class="container-respostas-pergunta-individual">
-        </div>        
-    </div>`)
-		);
-
-		corpo.innerHTML += `
-    <div class="pagina-de-um-quizz">
-      <div class="container-foto-de-capa-quizz">
-          <img src="${quizAtual.image}"/>
-          <span>${quizAtual.title}</span>
-      </div>
-      ${caixaDeRespostas}
-
-    <div class="container-fim-quizz hidden">
-        <div class="container-resultado" data-identifier="quizz-result">
-            <h1 class="tituloFimQuizz" >${porcentagemDeAcertos}% de acerto: ${quizzAtual.levels[0].title}</h1>
-        </div>
-        <div class="imagem-e-descricao">
-            <img src=${quizzAtual.levels[0].image}"/>
-            <p class= "textoQuizz">${quizzAtual.levels[0].text}
-            </p>
-        </div>
-        <button class="reiniciar-quizz" onclick="reiniciarQuizz(${quizzAtual.id})">
-            Reiniciar Quizz
-        </button>
-        <button  id='fullReset' class="voltar-home"  onclick="voltarTela('.corpo-pagina-inicial', '.pagina-de-um-quizz')">
-            Voltar para home
-        </button>
-    </div>
-  </div> 
-  `
-
+	corpo.innerHTML += `
+	<div class="pagina-de-um-quizz">
+	  <div class="container-foto-de-capa-quizz">
+		  <img src="${quizzAtual.image}"/>
+		  <span>${quizzAtual.title}</span>
+	  </div>
+	  ${caixaDeRespostas}
+	  <div class="container-fim-quizz hidden">
+		  <div class="container-resultado" data-identifier="quizz-result">
+			  <h1 class="tituloFimQuizz" >${porcentagemDeAcertos}% de acerto: ${quizzAtual.levels[0].title}</h1>
+		  </div>
+		  <div class="imagem-e-descricao">
+			  <img src=${quizzAtual.levels[0].image}"/>
+			  <p class= "textoQuizz">${quizzAtual.levels[0].text}
+			  </p>
+		  </div>
+		  <button class="reiniciar-quizz" onclick="reiniciarQuizz(${quizzAtual.id})">
+			  Reiniciar Quizz
+		  </button>
+		  <button  id='fullReset' class="voltar-home"  onclick="voltarTela('.corpo-pagina-inicial', '.pagina-de-um-quizz')">
+			  Voltar para home
+		  </button>
+	  </div>
+	</div> 
+	`;
+}
 
 function mostrarTelaDeCriacao() {
 	const telaInicial = document.querySelector(".corpo-pagina-inicial");
@@ -81,26 +82,27 @@ function mostrarTelaDeCriacao() {
 	telaInicial.classList.add("hidden");
 }
 
-function voltarHome(){
-    document.location.reload(true);
+function voltarHome() {
+	document.location.reload(true);
 }
 
-function exibirRespostasQuizz(respostas,qtdDeCadaResposta){
-  let ARRAY_RESPOSTAS = [];
-  console.log(respostas);
+function exibirRespostasQuizz(respostas, qtdDeCadaResposta) {
+	let ARRAY_RESPOSTAS = [];
+	console.log(respostas);
 
-  respostas.forEach( (item) => {
-    ARRAY_RESPOSTAS.push(
-    `<div class="container-resposta-indivual ${(item.isCorrectAnswer === true) ? "resposta-correta" : "resposta-errada"}        "onclick="selecionarResposta(this, ${qtdDeCadaResposta})" data-identifier="answer">
+	respostas.forEach((item) => {
+		ARRAY_RESPOSTAS.push(
+			`<div class="container-resposta-indivual ${
+				item.isCorrectAnswer === true ? "resposta-correta" : "resposta-errada"
+			}        "onclick="selecionarResposta(this, ${qtdDeCadaResposta})" data-identifier="answer">
             <img src="${item.image}"/>
             <span>${item.text}</span>
-     </div>`);
-  });
-  console.log(ARRAY_RESPOSTAS);
-  ARRAY_RESPOSTAS.sort(randomizador);
-  return ARRAY_RESPOSTAS;
-
-
+     </div>`
+		);
+	});
+	console.log(ARRAY_RESPOSTAS);
+	ARRAY_RESPOSTAS.sort(randomizador);
+	return ARRAY_RESPOSTAS;
 }
 
 function erroAxios() {
@@ -108,4 +110,4 @@ function erroAxios() {
 		"Houve uma falha na comunicação com o servidor, tente novamente mais tarde"
 	);
 	window.location.reload();
-	}
+}
